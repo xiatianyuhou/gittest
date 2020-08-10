f9fd9ed9d32ebb7989f8cb0bb0d7848500ce6945
朱春齐 <554458668@qq.com>
2020-08-10 14:58:17 +0800

    push and push 练习

--- src/com/atguigu/GitTest.java
+++ src/com/atguigu/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git练习666");
         System.out.println("git练习dev");
+        System.out.println("pull and push 练习");
     }
 }
